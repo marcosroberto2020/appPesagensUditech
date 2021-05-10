244d38cacefb5b9c9d27ba12dec073ad5bb6f464
Marcos <marcosrobertocc@gmail.com>
2021-05-10 05:28:17 -0300

    criadopool

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
-async function connect() {
-    if (global.connection && global.connection.state !== 'disconnected')
-        return global.connection;
 
-    const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://uditech01:Carvalho02@mysql743.umbler.com:41890/appuditech01");
-    console.log("Conectou no MySQL!");
-    global.connection = connection;
-    return connection;
+    /*     if (global.connection && global.connection.state !== 'disconnected')
+            return global.connection;
+    
+        const mysql = require("mysql2/promise");
+        const connection = await mysql.createConnection("mysql://uditech01:Carvalho02@mysql743.umbler.com:41890/appuditech01");
+        console.log("Conectou no MySQL!");
+        global.connection = connection;
+        return connection;
+     */
+    var mysql = require('mysql');
+    var pool = mysql.createPool({
+        "user": "uditech01",
+        "password": "Carvalho02",
+        "database": "appuditech01",
+        "host": "mysql743.umbler.com",
+        "port": 41890
+    });
 
-}
-module.exports  = {connect};
+
+module.exports = { pool };
 
--- src/controllers/primeira-controller.js
+++ src/controllers/primeira-controller.js
@@ -1,24 +1,27 @@
 'use strict';
 
-const conectar = require('../../db');
+const mysql = require('../../db').pool;
 
-exports.get = async(req, res, next) => {
-    try {
-        const conn = await conectar.connect();
-        const strSql = " SELECT pesagem.Pes_Codigo, pesagem.Pes_DataSaida, prd_Descricao AS produto, pesagem.pes_placa, " +
-                         "pesagem.Pes_PesoSaida, Pes_PesoEntrada, Pes_PesoSaida-Pes_PesoEntrada AS PesoLiquido, dbo_tb_cliente.Nome " + 
-                         "as Cliente FROM (dbo_tb_cliente INNER JOIN " +
-                          "(produto INNER JOIN pesagem ON produto.prd_codigo = pesagem.Cad_CodigoProduto) " +
-                          "ON dbo_tb_cliente.CodCliente = pesagem.Cad_Cliente) INNER JOIN motoristas ON " +
-                          "pesagem.Pes_CodMotorista = motoristas.CodMotorista";
-    
-        
-        const [rows] = await conn.query(strSql);     
-        res.status(200).send(rows);
-    } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição' 
-        });
-    }
+const strSql = " SELECT pesagem.Pes_Codigo, pesagem.Pes_DataSaida, prd_Descricao AS produto, pesagem.pes_placa, " +
+    "pesagem.Pes_PesoSaida, Pes_PesoEntrada, Pes_PesoSaida-Pes_PesoEntrada AS PesoLiquido, dbo_tb_cliente.Nome " +
+    "as Cliente FROM (dbo_tb_cliente INNER JOIN " +
+    "(produto INNER JOIN pesagem ON produto.prd_codigo = pesagem.Cad_CodigoProduto) " +
+    "ON dbo_tb_cliente.CodCliente = pesagem.Cad_Cliente) INNER JOIN motoristas ON " +
+    "pesagem.Pes_CodMotorista = motoristas.CodMotorista";
+
+//finalizar essa parte de conexao com o pool, retirar do try cathc conforme video https://www.youtube.com/watch?v=642J5YzLXDk&ab_channel=Maransatto
+
+exports.get = (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({ error: error })
+        }
+        conn.query(strSql,
+            (error, resultado, field) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({response: resultado})
+            } );
+    });
 }
-
+    
